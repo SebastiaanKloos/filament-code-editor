4da167ffc33d5dc5fb5869781003b46c49e9a50e
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-04-19 10:17:02 +0200

    Fixed the namespace

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace SebastiaanKloos\FilamentCodeEditor\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use SebastiaanKloos\FilamentCodeEditor\FilamentCodeEditorServiceProvider;
 
 class TestCase extends Orchestra
